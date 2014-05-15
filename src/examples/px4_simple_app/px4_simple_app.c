57b3d099bd8594916adfed27d9f6d744ac6c7f10
Steven Lu <stevenlu443@gmail.com>
2014-05-15 17:06:10 -0400

    let's see if this basic stuff can get it to do what I want

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -53,33 +53,45 @@ __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
 int px4_simple_app_main(int argc, char *argv[])
 {
-	printf("Hello Sky!\n");
+	printf("Initializing /dev/ttyS1...\n");
+
 	int uartfd = open("/dev/ttyS1", O_RDWR | O_NOCTTY); // TELEM2 port
 	assert(uartfd >= 0);
+	// manage terminal settings
+	int termios_state_ttyS1;
+	struct termios existing_config_ttyS1;
+	// get existing terminal config and store it.
+	assert((termios_state_ttyS1 = tcgetattr(uartfd, &existing_config_ttyS1)) >= 0);
+	struct termios config_ttyS1;
+	// duplicate into the new config
+	tcgetattr(uartfd, &config_ttyS1);
+	// memcpy(config_ttyS1, existing_config_ttyS1);
+
+	// clear ONLCR flag
+	config_ttyS1.c_oflag &= ~ONLCR;
+	// set baud rate
+	assert(cfsetispeed(&config_ttyS1, B460800) < 0 || cfsetospeed(&config_ttyS1, B460800) < 0);
+	// go ahead and set the config i am setting up
+	assert((termios_state_ttyS1 = tcsetattr(uartfd, TCSANOW, &config_ttyS1)) >= 0);
 
 	/* subscribe to sensor_combined topic */
 	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
 	orb_set_interval(sensor_sub_fd, 20);
 
-	/* advertise attitude topic */
-	struct vehicle_attitude_s att;
-	memset(&att, 0, sizeof(att));
-	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
-
 	/* one could wait for multiple topics with this technique, just using one here */
 	struct pollfd fds[] = {
 		{ .fd = sensor_sub_fd,   .events = POLLIN },
 		/* there could be more file descriptors here, in the form like:
-		 * { .fd = other_sub_fd,   .events = POLLIN },
-		 */
+		{ .fd = other_sub_fd,   .events = POLLIN },
+		*/
 	};
 
 	int error_counter = 0;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5000; i++) {
 		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
 		int poll_ret = poll(fds, 1, 1000);
-	 
+
 		/* handle the poll result */
 		if (poll_ret == 0) {
 			/* this means none of our providers is giving us data */
@@ -93,7 +105,7 @@ int px4_simple_app_main(int argc, char *argv[])
 			}
 			error_counter++;
 		} else {
-	 
+
 			if (fds[0].revents & POLLIN) {
 				/* obtained data for the first file descriptor */
 				struct sensor_combined_s raw;
@@ -103,18 +115,20 @@ int px4_simple_app_main(int argc, char *argv[])
 					(double)raw.accelerometer_m_s2[0],
 					(double)raw.accelerometer_m_s2[1],
 					(double)raw.accelerometer_m_s2[2]);
-
-				/* set att and publish this information for other apps */
-				att.roll = raw.accelerometer_m_s2[0];
-				att.pitch = raw.accelerometer_m_s2[1];
-				att.yaw = raw.accelerometer_m_s2[2];
-				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
+				fprintf(uartfd, "[px4_simple_app] -- on uart -- Accelerometer:\t%8.4f\t%8.4f\t%8.4f\n",
+					(double)raw.accelerometer_m_s2[0],
+					(double)raw.accelerometer_m_s2[1],
+					(double)raw.accelerometer_m_s2[2]);
 			}
 			/* there could be more file descriptors here, in the form like:
-			 * if (fds[1..n].revents & POLLIN) {}
-			 */
+			if (fds[1..n].revents & POLLIN) {}
+			*/
 		}
 	}
+
+	// cleanup serial port
+	tcsetattr(uartfd, TCSANOW, &existing_config_ttyS1);
+	close(uartfd);
 
 	return 0;
 }
